a0a26669e0556ed6ebce9f972c0d5146848a0abe
Chiara Nuñez <chiaraani@gmail.com>
2026-07-14 19:32:18 +0100

    LSP

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.keymap.set('n', '<C-f>', function()
+        vim.lsp.buf.format()
+      end, { desc = 'Format with LSP' })
+
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
+
+      vim.lsp.enable('vue_ls')
+      vim.lsp.enable('solargraph')
+      vim.lsp.enable('rubocop')
+      vim.lsp.enable('lua_ls')
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" } }
+          }
+        }
+      })
+    end
+  }
+}
