e749f33ef6fc355e905df4959c84b75ee5d1b396
Chiara Nuñez <chiaraani@gmail.com>
2026-07-16 12:45:31 +0100

    fix: treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,11 +5,12 @@ return {
     build = ':TSUpdate',
     config = function()
       local config = require("nvim-treesitter.config")
-      local preferred_langs = {
-        "lua", "javascript", "html", "css", "ruby", "vue", "vimdoc", "vim", "sql"
+      local langs = {
+        "lua", "html", "css", "ruby", "vue", "vimdoc", "vim", "sql",
+        "json", "javascript", "yaml"
       }
       config.setup({
-        ensure_installed = preferred_langs,
+        ensure_installed = langs,
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false
@@ -17,9 +18,12 @@ return {
         indent = { enable = true }
       })
 
+      local autocmd_langs = {
+        "lua", "html", "css", "ruby", "vue", "vimdoc", "vim", "sql"
+      }
 
       vim.api.nvim_create_autocmd('FileType', {
-        pattern = preferred_langs,
+        pattern = autocmd_langs,
         callback = function() vim.treesitter.start() end,
       })
     end
