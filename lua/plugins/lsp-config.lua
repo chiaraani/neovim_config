bf6bb0e44a78b0426b20a62e82170fc744a9be4c
Chiara Nuñez <chiaraani@gmail.com>
2026-07-17 18:15:20 +0100

    show hidden files in neo-tree

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
       vim.lsp.enable('vue_ls')
       vim.lsp.enable('yamlls')
       vim.lsp.enable('solargraph')
-      vim.lsp.enable('rubocop')
+      -- vim.lsp.enable('rubocop')
       vim.lsp.enable('vimls')
       vim.lsp.enable('lua_ls')
       vim.lsp.config("lua_ls", {
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,8 +7,18 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
+    lazy = false,                    -- neo-tree will lazily load itself
     config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        }
+      }
+
       vim.keymap.set('n', '<leader>e', function()
         vim.cmd(":Neotree toggle reveal left")
       end)
